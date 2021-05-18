6019b26ce0c65cba5db74b6e99ba769779972cc2
Vatsal Chauhan <chauhan.vats3@gmail.com>
2021-05-18 15:00:31 +0530

    added nodemailer

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -4,21 +4,8 @@ const nodemailer = require("nodemailer");
 exports.handler = async function(event, context) {
   const params = event.queryStringParameters;
   const dataset = JSON.parse(params.dataset);
-
-  const jsonFileName = dataset.passport.number + ".json";
-  const imageFileName = dataset.passport.number + ".png";
   let transporter;
 
-  //const fsPromises = require('fs').promises;
-  /* const cFormHTML = await fsPromises.readFile('./static/cformImage.html')
-        .catch((err) => console.error('Failed to read file', err)); */
-
-  /*  console.log("cformhtml : " + typeof cFormHTML.toString()) */
-
-  /*  const cFormImage = await nodeHtmlToImage({
-         html: cFormHTML.toString(),
-         content: { basic: dataset.basic, misc: dataset.misc, passport: dataset.passport, visa: dataset.visa }
-     }); */
   transporter = nodemailer.createTransport({
     host: process.env.ZOHO_HOST,
     port: process.env.ZOHO_PORT,
@@ -30,23 +17,11 @@ exports.handler = async function(event, context) {
   });
 
   let info = await transporter.sendMail({
-    from: process.env.ZOHO_EMAIL_CONTACT,
+    from: process.env.ZOHO_EMAIL,
     to: process.env.ZOHO_AUTH_USER,
-    subject: `[C-Form] ${dataset.basic.f_name} ${dataset.basic.l_name}`,
-    text: "Please Find an Attachment",
-    attachments: [
-      {
-        filename: jsonFileName,
-        content: JSON.stringify(dataset)
-      }
-    ]
+    subject: ``,
+    text: ""
   });
-
-  /* , {
-            filename: imageFileName,
-            content: cFormImage,
-            contentType: 'image/png'
-        } */
 
   console.log("Message sent: %s", info);
 
